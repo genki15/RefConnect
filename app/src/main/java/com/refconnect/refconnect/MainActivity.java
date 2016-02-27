ae780876ac944959e46e440a1e20d2e22f5367c4
Genki Miyauchi <genki.miyauchi@kcl.ac.uk>
2016-02-27 21:23:15 +0000

    added fire base permission

--- app/src/main/java/com/refconnect/refconnect/MainActivity.java
+++ app/src/main/java/com/refconnect/refconnect/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.volunteer_search);
+        setContentView(R.layout.activity_main);
 
     }
 
