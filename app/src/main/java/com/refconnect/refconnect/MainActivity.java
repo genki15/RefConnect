520454d66559bc318be3a20b5ced6b8b6c242878
Genki Miyauchi <genki.miyauchi@kcl.ac.uk>
2016-02-28 01:47:28 +0000

    UI for start and post screen

--- app/src/main/java/com/refconnect/refconnect/MainActivity.java
+++ app/src/main/java/com/refconnect/refconnect/MainActivity.java
@@ -6,21 +6,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button postButton = (Button) findViewById(R.id.search_button);
-        Button browseButton = (Button) findViewById(R.id.volunteer_button);
-
 
     }
 
@@ -30,8 +32,9 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
 
         }else if(view.getId() == R.id.volunteer_button){
-            Intent intent = new Intent(this, PostActivity.class);
+            Intent intent = new Intent(this, VolunteerSearch.class);
             startActivity(intent);
         }
     }
+
 }
--- app/src/main/java/com/refconnect/refconnect/VolunteerSearch.java
+++ app/src/main/java/com/refconnect/refconnect/VolunteerSearch.java
@@ -1,16 +1,74 @@
 package com.refconnect.refconnect;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Created by genki on 16/02/27.
  */
 public class VolunteerSearch extends AppCompatActivity{
 
+    private double lat = 35.670930;
+    private double lng = 139.50249;
+    private ImageView map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.volunteer_search);
+
+        map = (ImageView) findViewById(R.id.map);
+
+        String imageUrl = "http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng
+                + "&zoom=10"
+                + "&size=640x640"
+                + "&scale=1"
+                + "&key=AIzaSyDUKe-EJql0HVzkwMvjXvu5B6CmCSmHpp4";
+
+        try {
+            URL url = new URL(imageUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        new LoadImage().execute(imageUrl);
+    }
+
+    private class LoadImage extends AsyncTask<String, String, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        protected Bitmap doInBackground(String... args) {
+
+            Bitmap bitmap = null;
+            if(args.length == 1){
+                Log.i("doInBack 1", "length = 1 ");
+
+                try {
+                    bitmap = BitmapFactory.decodeStream((InputStream) new URL(args[0]).getContent());
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return bitmap;
+        }
+        protected void onPostExecute(Bitmap image) {
+            if(image != null){
+                map.setImageBitmap(image);
+
+            }
+        }
     }
 }
